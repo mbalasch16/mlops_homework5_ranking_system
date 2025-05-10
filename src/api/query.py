8a7e31063e0000033045a0d5c1e7b98126c8907b
EC2 Default User <ec2-user@ip-172-31-20-56.ec2.internal>
2025-05-10 02:03:19 +0000

    adding code

--- src/api/query.py
+++ src/api/query.py
@@ -4,6 +4,7 @@ from src.models.query import RAGRequest, RAGResponse
 
 router = APIRouter()
 
+#added include_in_schema
 @router.post("/similar_responses", response_model=RAGResponse,include_in_schema=False)
 def get_similar_responses(request: RAGRequest):
     results = retriever.get_similar_responses(request.question)
